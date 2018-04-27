55bc15468ecd33595da619746c7dcddf98c6ea9d
Pierre Cavalet <pcavalet@kaliop.com>
2018-04-27 16:25:11 +0200

    chai integration + tests

--- services/calc.js
+++ services/calc.js
@@ -1,12 +1,20 @@
 function sum(a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    throw Error("One parameter is not an integer");
+  }
+
   return a + b;
 }
 
 function asynchronousSum(a, b) {
   return new Promise((resolve, reject) => {
     setTimeout(function() {
-      const result = sum(a, b);
-      resolve(result);
+      try {
+        const result = sum(a, b);
+        resolve(result);
+      } catch (err) {
+        reject(err);
+      }
     }, 10);
   });
 }
--- test/calc/test.js
+++ test/calc/test.js
@@ -1,4 +1,9 @@
-const assert = require("assert");
+const chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
 const calc = require("../../services/calc");
 
 describe("Calc", function() {
@@ -6,22 +11,30 @@ describe("Calc", function() {
   describe("# sum()", function() {
     it("should return 3 with paramaters 1 and 2", function() {
       const result = calc.sum(1, 2);
-      assert.equal(result, 3);
+      expect(result).to.equal(3);
+    });
+
+    it("should throw an error without parameter", function() {
+      expect(calc.sum).to.throws();
+    });
+
+    it("should throw an error with string parameter", function() {
+      expect(() => calc.sum("1", 2)).to.throws();
     });
   });
 
   // asynchronous sum
   describe("# asynchronousSum()", function() {
-    it("should return 3 with paramaters 1 and 2", function(done) {
-      calc
-        .asynchronousSum(1, 2)
-        .then(result => {
-          assert.equal(result, 3);
-          done();
-        })
-        .catch(err => {
-          done(err);
-        });
+    it("should return 3 with paramaters 1 and 2", function() {
+      return expect(calc.asynchronousSum(1, 2)).to.eventually.equal(3);
+    });
+
+    it("should return 3 with paramaters 1 and 2", function() {
+      return expect(calc.asynchronousSum()).to.be.rejected;
+    });
+
+    it("should throw an error with string parameter", function() {
+      return expect(calc.asynchronousSum("1", 2)).to.be.rejected;
     });
   });
 });
