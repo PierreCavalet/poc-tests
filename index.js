d82cdaa8807aea5d77a801683bf11a3eb711c561
Pierre Cavalet <pcavalet@kaliop.com>
2018-04-27 14:29:12 +0200

    add a add route

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var express = require("express");
 var app = express();
 
+var calc = require("./services/calc.js");
+
 app.get("/", function(req, res) {
   res.send("Hello World!");
+});
+
+app.get("/add/:input1/:input2", function(req, res) {
+  var input1 = parseInt(req.params.input1);
+  var input2 = parseInt(req.params.input2);
+
+  var result = calc.sum(input1, input2);
+
+  res.send(result.toString());
 });
 
 app.listen(3000, function() {
