47268c371b4faf802113217cc84f3ee78a15d97f
Pierre Cavalet <pcavalet@kaliop.com>
2018-04-30 17:36:15 +0200

    puppeteer poc

---
+++ test/puppeteer/test.js
@@ -0,0 +1,26 @@
+const puppeteer = require("puppeteer");
+const chai = require("chai");
+const expect = chai.expect;
+
+describe("puppeteer", function() {
+  describe("add", function() {
+    it("should print 5", async function() {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+
+      await page.goto("http://localhost:3000/form");
+      await page.waitFor("input[name=input1]");
+      await page.click("input[name=input1]");
+      await page.keyboard.type("2");
+      await page.click("input[name=input2]");
+      await page.keyboard.type("3");
+      await page.click("input[type=submit]");
+      await page.waitFor("#result");
+      const result = await page.evaluate(
+        () => document.querySelector("#result").textContent
+      );
+      expect(result).to.eql("5");
+      await page.close();
+    });
+  });
+});
